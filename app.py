24f0a2bc4916e3d084beac8eab162bacede4f3d8
ByteDance <zzuzhangwen@aliyun.com>
2026-02-12 15:43:02 +0800

    aaa

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.tools.file_operation import write_file, read_file
 from control.notifier import Notifier
 from resources.tools.console_input import get_input
 
-TEST_CASE_1="https://mp.weixin.qq.com/s/qbXm1Vq7dc_2KPOJerZUqw 对网页内容做总结，提取摘要，输出pdf文件，格式美观"
+TEST_CASE_1="https://mp.weixin.qq.com/s/qbXm1Vq7dc_2KPOJerZUqw 对网页内容做总结，提取摘要，输出pdf文件，格式美观。"
 TEST_CASE_2="我想去旅游，帮我做一下规划吧"
 TEST_CASE_3="输出10个1"
 TEST_CASE_4="我需要生成一份公司周报的ppt，用来在会议上演示" # Badcase 如果说信息在文件内的话就会不停的要求给文字内容，因为不能读取 - 已解决。
@@ -30,8 +30,8 @@ notifier = Notifier(context_manager)
 DEFAULT_TOOLS_LIST = tool_executer.list_tools()
 print(f"Available tools: {DEFAULT_TOOLS_LIST}")
 # for test only
-context_manager.add_available_resources({"公司信息，包含周报公司名称、汇报时间周期及核心内容模块": ResourceReference(description="公司信息，包含周报公司名称、汇报时间周期及核心内容模块", URI="https://www.my_company.com/report", type="from_memorybase")})
-context_manager.add_available_resources({"需要解决的题目截图": ResourceReference(description="需要解决的题目截图", URI="image.png", type="from_memorybase")})
+# context_manager.add_available_resources({"公司信息，包含周报公司名称、汇报时间周期及核心内容模块": ResourceReference(description="公司信息，包含周报公司名称、汇报时间周期及核心内容模块", URI="https://www.my_company.com/report", type="from_memorybase")})
+# context_manager.add_available_resources({"需要解决的题目截图": ResourceReference(description="需要解决的题目截图", URI="image.png", type="from_memorybase")})
 
 
 
@@ -39,7 +39,7 @@ context_manager.add_available_resources({"需要解决的题目截图": Resource
 claim_agent = ClaimerAgent(notifier, context_manager)
 plan_agent = PlannerAgent(context_manager, notifier)
 agent_factory = AgentFactory(context_manager,DEFAULT_TOOLS_LIST, tool_executer, shell)
-sys_prompt, msg = claim_agent.run(TEST_CASE_9)
+sys_prompt, msg = claim_agent.run(TEST_CASE_1)
 is_accomplished = plan_agent.run()
 
 while not is_accomplished:
